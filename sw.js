74d06af5daab0089a2bbf60f323f706d78b2f4d1
yhw-study <loveyanghw@gmail.com>
2026-09-21 01:21:55 +0900

    UP

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='study-pwa-v10';
-const CORE=['./','./index.html','./study.html','./lecture.html','./sleep.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE='study-pwa-v11';
+const CORE=['./','./index.html','./study.html','./lecture.html','./sleep.html','./record.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('notificationclick',e=>{
