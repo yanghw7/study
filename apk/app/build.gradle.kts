d0216d5871cb2ed6b934b1649ac389d3cb298190
yanghw7 <loveyanghw@gmail.com>
2026-09-23 01:55:39 +0900

    v17 OAuth fix

--- apk/app/build.gradle.kts
+++ apk/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.min.journal"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "1.16"
+        versionCode = 17
+        versionName = "1.17"
     }
 
     buildTypes {
