f52047d9e7158de92f51515e2d5b2053f1c29929
yanghw7 <loveyanghw@gmail.com>
2026-09-23 01:38:55 +0900

    v16 latest APK

--- apk/app/build.gradle.kts
+++ apk/app/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.min.journal"
         minSdk = 26
         targetSdk = 35
-        versionCode = 15
-        versionName = "1.15"
+        versionCode = 16
+        versionName = "1.16"
     }
 
     buildTypes {
